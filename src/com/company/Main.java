e8d51842bf7b7867ae5fa6b8153e9c11580e8971
Paguro <ivankosaev2006@gmail.com>
2023-09-02 21:44:23 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,58 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args){
+        int min_sum = 2000;
+        int quantity = 0;
+
+        int min_37 = 10001;
+        int max_73 = 0;
+
+        int[] numbers = new int[1000];
+
+        try (BufferedReader reader = new BufferedReader(new FileReader("numbers.txt"));){
+            int number;
+            for(int i = 0; i < 1000; i++){
+                number = Integer.parseInt(reader.readLine());
+                numbers[i] = number;
+
+                if ((number % 37 == 0) && (number < min_37)) {
+                    min_37 = number;
+                }
+                if ((number % 73 == 0) && (number > max_73)) {
+                    max_73 = number;
+                }
+            }
+        }
+
+        catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }
+
+        for(int i = 0; i < 1000; i++){
+            if (min_37 < numbers[i] || numbers[i] < max_73){
+                if(i-1 >= 0 &&!(min_37 < numbers[i-1] || numbers[i-1] < max_73)){
+                    quantity++;
+                    int sum = numbers[i] + numbers[i-1];
+                    if (sum < min_sum){
+                        min_sum = sum;
+                    }
+                }
+                if (i + 1 < 1000 &&!(min_37 < numbers[i+1] || numbers[i+1] < max_73)){
+                    quantity++;
+                    int sum = numbers[i] + numbers[i+1];
+                    if (sum < min_sum){
+                        min_sum = sum;
+                    }
+                }
+            }
+        }
+        System.out.println(min_sum);
+        System.out.println(quantity);
+    }
+}
